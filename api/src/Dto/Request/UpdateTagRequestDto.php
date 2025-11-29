50c7f83df43efa06c10b3dec1d1e073151ef957a
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-29 14:29:56 +0100

    add Assert NotBlank with allowNull true to update artist / tag request dtos

--- api/src/Dto/Request/UpdateTagRequestDto.php
+++ api/src/Dto/Request/UpdateTagRequestDto.php
@@ -2,9 +2,12 @@
 
 namespace App\Dto\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 readonly class UpdateTagRequestDto
 {
     public function __construct(
+        #[Assert\NotBlank(message: "Name should not be blank.", allowNull: true)]
         public ?string $name,
     ) {}
 }
