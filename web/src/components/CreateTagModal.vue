f39ee0d2ac57f6544c05e122aab88727a7b75955
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-06 22:35:33 +0100

    close modal on create artist / tag modal submit

--- web/src/components/CreateTagModal.vue
+++ web/src/components/CreateTagModal.vue
@@ -2,13 +2,20 @@
 import { createTag } from '@/services/api/tagClient';
 import { ref } from 'vue';
 
+const emit = defineEmits(['close']);
+
 const name = ref('');
+
+async function handleSubmit() {
+    await createTag({ name: name.value });
+    emit('close');
+}
 </script>
 
 <template>
     <div class="space-y-6">
         <h2 class="text-2xl font-bold text-center">Create New Tag</h2>
-        <form @submit.prevent="createTag({ name })" class="space-y-4">
+        <form @submit.prevent="handleSubmit" class="space-y-4">
             <div>
                 <label for="tag-name" class="label">
                     <span class="label-text text-base font-semibold">Tag Name</span>
