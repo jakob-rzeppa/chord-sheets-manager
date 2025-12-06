4dbb496f1c18b50121ba72e97fe2b2c2c430fc35
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-06 22:35:33 +0100

    close modal on create artist / tag modal submit

--- web/src/components/CreateArtistModal.vue
+++ web/src/components/CreateArtistModal.vue
@@ -2,13 +2,20 @@
 import { createArtist } from '@/services/api/artistClient';
 import { ref } from 'vue';
 
+const emit = defineEmits(['close']);
+
 const name = ref('');
+
+async function handleSubmit() {
+    await createArtist({ name: name.value });
+    emit('close');
+}
 </script>
 
 <template>
     <div class="space-y-6">
         <h2 class="text-2xl font-bold text-center">Create New Artist</h2>
-        <form @submit.prevent="createArtist({ name })" class="space-y-4">
+        <form @submit.prevent="handleSubmit" class="space-y-4">
             <div>
                 <label for="artist-name" class="label">
                     <span class="label-text text-base font-semibold">Artist Name</span>
